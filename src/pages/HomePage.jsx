1c1afcbc1d1002cab159ebd6971a401c9b99b4cb
ARUN KUMAR.A <arun@vydurya.in>
2025-10-21 13:00:11 +0530

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Hero from '../components/mainwebsiteComponent/Hero'
-import BrandSlide from '../components/mainwebsiteComponent/BrandSlide'
-import Features from '../components/mainwebsiteComponent/Features'
-
+import React from "react";
+import Hero from "../components/mainwebsiteComponent/Hero";
+import BrandSlide from "../components/mainwebsiteComponent/BrandSlide";
+import Features from "../components/mainwebsiteComponent/Features";
+import ScrollStack, { ScrollStackItem } from "../components/ui/ScrollStack";
+import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+import ButtonV1 from "../components/ButtonV1";
 
 const HomePage = () => {
   return (
     <>
-      <Hero/>
-      <BrandSlide/>
+      <Hero />
+      <BrandSlide />
       <Features/>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/domainPages/DomainPage.jsx
+++ src/pages/domainPages/DomainPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import PixelBlast from "../../components/ui/PixelBlast";
 
 const DomainPage = () => {
   return (
-    <div>DomainPage</div>
-  )
-}
+    <div>
+      <div className="w-full h-[40vh] relative bg-secondary mb-8">
+        <PixelBlast
+          variant="square"
+          pixelSize={5}
+          color="#00AEEE"
+          patternScale={3}
+          patternDensity={1.2}
+          pixelSizeJitter={0.5}
+          enableRipples
+          rippleSpeed={0.4}
+          rippleThickness={0.12}
+          rippleIntensityScale={1.5}
+          liquid={false}
+          liquidStrength={0.12}
+          liquidRadius={1.2}
+          liquidWobbleSpeed={5}
+          speed={0.6}
+          edgeFade={0.25}
+          transparent
+        />
+      </div>
+    </div>
+  );
+};
 
-export default DomainPage
+export default DomainPage;
