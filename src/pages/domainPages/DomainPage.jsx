e95d5f78083a309e90044d825c73dffa3f535eec
ARUN KUMAR.A <arun@vydurya.in>
2025-10-21 17:08:01 +0530

    commit

--- src/pages/domainPages/DomainPage.jsx
+++ src/pages/domainPages/DomainPage.jsx
@@ -77,21 +77,50 @@ const DomainPage = () => {
         </div>
       </div>
 
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center">
         <ScrollFloat
           animationDuration={1}
           ease='back.inOut(2)'
-          scrollStart='center bottom+=50%'
+          scrollStart='top bottom'
           scrollEnd='bottom bottom-=40%'
           stagger={0.03} 
           textClassName="" 
         >
           Choose from the most popular domains
         </ScrollFloat>
-        <div>
-          <ul>
-            <img src="" alt="" />
-
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-10">
+          <ul className="flex flex-col border-[1px] border-gray-500 rounded-lg px-3 py-3 space-y-2 w-[90%]">
+            <img src="/images/com.png" alt="com" className="w-34"/>
+            <p >Build trust with the best-known domain names.</p>
+            <div className="flex justify-start w-full gap-3 items-center px-1">
+              <p className="text-sm text-gray-800 line-through">US$ 6.99/1st yr</p>
+              <div className="bg-pink-600 text-white rounded-full px-2 py-1">Save 80%</div>
+            </div>
+            <div className="flex justify-start w-full gap-3 items-center px-1">US$<p className="text-2xl font-semibold">2.39</p>/year</div>
+            
+            <button className="bg-violet-600 text-background w-full rounded-lg h-12 text-xl">Register</button>
+          </ul>
+          <ul className="flex flex-col border-[1px] border-gray-500 rounded-lg px-3 py-3 space-y-2 w-[90%]">
+            <img src="/images/com.png" alt="com" className="w-34"/>
+            <p >Build trust with the best-known domain names.</p>
+            <div className="flex justify-start w-full gap-3 items-center px-1">
+              <p className="text-sm text-gray-800 line-through">US$ 6.99/1st yr</p>
+              <div className="bg-pink-600 text-white rounded-full px-2 py-1">Save 80%</div>
+            </div>
+            <div className="flex justify-start w-full gap-3 items-center px-1">US$<p className="text-2xl font-semibold">2.39</p>/year</div>
+            
+            <button className="bg-violet-600 text-background w-full rounded-lg h-12 text-xl">Register</button>
+          </ul>
+          <ul className="flex flex-col border-[1px] border-gray-500 rounded-lg px-3 py-3 space-y-2 w-[90%]">
+            <img src="/images/com.png" alt="com" className="w-34"/>
+            <p >Build trust with the best-known domain names.</p>
+            <div className="flex justify-start w-full gap-3 items-center px-1">
+              <p className="text-sm text-gray-800 line-through">US$ 6.99/1st yr</p>
+              <div className="bg-pink-600 text-white rounded-full px-2 py-1">Save 80%</div>
+            </div>
+            <div className="flex justify-start w-full gap-3 items-center px-1">US$<p className="text-2xl font-semibold">2.39</p>/year</div>
+            
+            <button className="bg-violet-600 text-background w-full rounded-lg h-12 text-xl">Register</button>
           </ul>
         </div>
       </div>
