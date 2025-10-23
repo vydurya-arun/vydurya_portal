22522474fade7755f4beb1360481da0062520689
ARUN KUMAR.A <arun@vydurya.in>
2025-10-23 11:57:59 +0530

    commi

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../../components/mainwebsiteComponent/Hero";
 import BrandSlide from "../../components/mainwebsiteComponent/BrandSlide";
-import Features from "../../components/mainwebsiteComponent/Features";
 import NewFeature from "../../components/mainwebsiteComponent/NewFeature";
 import PricingCard from "../pricing/PricingCard";
 
